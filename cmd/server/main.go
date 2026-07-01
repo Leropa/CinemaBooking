7b67a789eb8be5173e87679b34454dd341e55d17
Leropa <neznayu.2001@internet.ru>
2026-07-01 12:35:22 +0500

    add main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,20 @@
 package main
 
 import (
+	"cinema_booking/db"
+	"net/http"
+
 	"github.com/go-chi/chi/v5"
 )
+
+func main() {
+	db.Init()
+	defer db.DB.Close()
+
+	r := chi.NewRouter()
+
+	r.Get("/api/v1")
+
+	http.ListenAndServe(":8000", r)
+
+}
